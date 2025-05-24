cc4d303efdaf7168cd81d6988e7b12e680997c46
FabioAccavone <fabioaccavone90@gmail.com>
2025-05-24 11:55:08 +0200

    Inserimento ultimi Documenti

--- src/main.py
+++ src/main.py
@@ -5,13 +5,13 @@ from data_processing.prepare_chunks import prepare_contextualized_chunks
 from config.settings import CHUNK_SIZE, OVERLAP, PDF_FOLDER
 
 if __name__ == "__main__":
-    processed_results_with_sources = prepare_contextualized_chunks(
-        PDF_FOLDER, CHUNK_SIZE, OVERLAP, chunk_limit_per_minute=10, window_size=10
-    )
+    #processed_results_with_sources = prepare_contextualized_chunks(
+    #    PDF_FOLDER, CHUNK_SIZE, OVERLAP, chunk_limit_per_minute=10, window_size=10
+    #)
 
     # Inizializza Pinecone e BM25 con i chunk processati
-    add_pdfs_to_pinecone(processed_results_with_sources)
-    build_bm25_index(processed_results_with_sources)
+    # add_pdfs_to_pinecone(processed_results_with_sources)
+    # build_bm25_index(processed_results_with_sources)
 
     # Avvia l’interfaccia Gradio
     start_interface()
